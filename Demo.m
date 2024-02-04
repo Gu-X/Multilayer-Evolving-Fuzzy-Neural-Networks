d878229500843327887b253dd1a8bc4ea0ba868f
XiaoweiGu <xw.gu@hotmail.com>
2024-02-04 10:30:02 +0000

    Update Demo.m

--- Demo.m
+++ Demo.m
@@ -13,14 +13,14 @@ mode='R' %% Regression
 Wo=3;
 niter=200; %% number of iteration
 lr=1; %% learning rate
-[Ye]=SOFNN2(data0,y0,data1,mode,Wo,niter,lr);
+[Ye]=MEFNN2(data0,y0,data1,mode,Wo,niter,lr); % train a two-layer MEFNN
 RMSE=sqrt(mean((Ye-y1).^2)) % root mean square error
 NDEI=sqrt(mean((Ye-y1).^2))/std(y1) % non-dimensional error index
 
 %% Example: Classification
 load ExampleData_classification
 mode='C' %% Classification
-[Ye]=SOFNN2(data0,y0,data1,mode,Wo,niter,lr);
+[Ye]=MEFNN2(data0,y0,data1,mode,Wo,niter,lr);
 CL=length(unique(y0));
 CM=confusionmat(y1,Ye) % confusion matrix
 Accuracy=sum(CM.*eye(CL),'all')/sum(CM,'all') % classification accuracy
