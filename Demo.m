1ed003faa5fcd4bc52bd7507c01860fbae6fbc17
XiaoweiGu <xw.gu@hotmail.com>
2024-02-04 10:22:42 +0000

    Add files via upload

---
+++ Demo.m
@@ -0,0 +1,26 @@
+clear all
+clc
+close all
+%% Example: Regression
+load ExampleData_regression
+%%
+% data0: training data
+% y0: ground truth of data0
+% data1: testing data
+% y1: group truth of data1
+%%
+mode='R' %% Regression
+Wo=3;
+niter=200; %% number of iteration
+lr=1; %% learning rate
+[Ye]=SOFNN2(data0,y0,data1,mode,Wo,niter,lr);
+RMSE=sqrt(mean((Ye-y1).^2)) % root mean square error
+NDEI=sqrt(mean((Ye-y1).^2))/std(y1) % non-dimensional error index
+
+%% Example: Classification
+load ExampleData_classification
+mode='C' %% Classification
+[Ye]=SOFNN2(data0,y0,data1,mode,Wo,niter,lr);
+CL=length(unique(y0));
+CM=confusionmat(y1,Ye) % confusion matrix
+Accuracy=sum(CM.*eye(CL),'all')/sum(CM,'all') % classification accuracy
